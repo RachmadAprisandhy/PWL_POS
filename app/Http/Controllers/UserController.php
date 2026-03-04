523ea26f32d5c5e6854ac0063b6cbb99c3b3f4e4
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2026-03-05 04:22:49 +0700

    commit 4 jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,23 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
- public function index()
-{
-   $users = UserModel::where('level_id', 2)->count();
-   return view('user', ['data' => $users]);
-}
+    public function index()
+    {
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
+         $user->save();
+
+         $user->wasChanged();
+         $user->wasChanged('username');
+         $user->wasChanged('username', 'level_id');
+         $user->wasChanged('nama');
+         dd($user->wasChanged(['username', 'nama']));
+    }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,8 +1,17 @@
-<h1>Data User</h1>
-
-<div style="border:1px solid black; width:200px; padding:10px;">
-    <p>Jumlah Pengguna</p>
-    <div style="border:1px solid black; padding:5px;">
-        {{ $data }}
-    </div>
-</div>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+    </table>
+</body>
